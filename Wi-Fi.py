b2f9c9deda7c4b0e000c2a5c8c050bf41117edea
Gabriel Bogni <57570492+GabBogni@users.noreply.github.com>
2023-01-30 09:41:17 -0300

    Update Wi-Fi.py

--- Wi-Fi.py
+++ Wi-Fi.py
@@ -7,19 +7,20 @@ wlan = network.WLAN(network.STA_IF)
 if not wlan.active():
   wlan.active(True)
 
+if not wlan.isconnected():
+  wlan.connect('wifi','password') # Realiza infinitas tentativas após chamar a função connect()
+ 
 while not wlan.isconnected():
-  wlan.connect('Gabriel','gabbog98')
-  print('Tentando se conectar a rede...')
-  sleep(1)
-  print('Codigo de estado de conexao '+str(wlan.status()))
+    print('Tentando se conectar a rede...')
+    sleep(1)
+    print('Codigo de estado de conexao '+str(wlan.status()))
  
 print('Conectado')
 print('Dados da interface de rede '+str(wlan.ifconfig()))
+
 print('Realizando uma consulta na API de adviceslip')
-
 response = urequests.get('https://api.adviceslip.com/advice')
-objeto_json = response.json()
-
-print('Conselho:'+objeto_json['slip']['advice'])
+dicionario_json = response.json()
+print('Conselho:'+dicionario_json['slip']['advice'])
 
 
